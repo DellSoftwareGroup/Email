1326726a3e44c485f3a9b595b1bde93525e03972
Edward Chong <edward@ed-mart.com>
2015-09-01 10:25:34 -0700

    initial commit.

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require 'Emogrifier.php';
 
 $widget = new Widget();
 
+$toolbar = '';
+
 if (isset($_GET['download'])) {
 	$info = pathinfo($_GET['page']);
 
@@ -11,23 +13,22 @@ if (isset($_GET['download'])) {
 	header("Content-Transfer-Encoding: Ascii");
 	header("Content-disposition: attachment; filename=\"{$info['basename']}\"");
 }
-
-$toolbar = <<<END
+else {
+	$toolbar = <<<END
 <div style="position: fixed; bottom: 0; right: 0; border: 1px solid #444; padding: 5px; background-color: #c0c0c0;">
 	<div>- <a href="?download">Download File</a></div>
 END;
 
-if(isset($_GET['no-process'])) {
-	$toolbar .= '<div>- <a href="?">Inline Processing</a></div>';
-	$toolbar .= '<div>- <strong>No Inline Processing</strong></div>';
-}
-else {
-	$toolbar .= '<div>- <strong>Inline Processing</strong></div>';
-	$toolbar .= '<div>- <a href="?no-process">No Inline Processing</a></div>';
-}
+	if (isset($_GET['no-process'])) {
+		$toolbar .= '<div>- <a href="?">Inline Processing</a></div>';
+		$toolbar .= '<div>- <strong>No Inline Processing</strong></div>';
+	} else {
+		$toolbar .= '<div>- <strong>Inline Processing</strong></div>';
+		$toolbar .= '<div>- <a href="?no-process">No Inline Processing</a></div>';
+	}
 
-$toolbar .= '</div>';
-
+	$toolbar .= '</div>';
+}
 
 $content = $widget->content();
 
